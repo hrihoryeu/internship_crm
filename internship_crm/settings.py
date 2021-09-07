7159a82912b52e77ef78c4eee626b5807c63439f
hrihoryeu <egoragrig@gmail.com>
2021-09-07 16:20:45 +0300

    docker to project connected

--- internship_crm/settings.py
+++ internship_crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hq=kobx(3pd1+gbvgg3c8d8sct#!imd701lfo*b+e0y-r75_l8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0',]
 
 
 # Application definition
