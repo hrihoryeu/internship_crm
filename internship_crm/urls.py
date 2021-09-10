1b627dd24dc4798ddc937c5dbaca8d78c5ebdfb3
hrihoryeu <egoragrig@gmail.com>
2021-09-10 13:59:03 +0300

    feat: added swagger to api

--- internship_crm/urls.py
+++ internship_crm/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,5 @@ urlpatterns = [
     path('api/car_showroom/', include('car_showroom.urls')),
     path('api/customer/', include('customer.urls')),
 ]
+
+urlpatterns += doc_urls
