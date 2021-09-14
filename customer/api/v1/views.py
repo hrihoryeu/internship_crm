59090e9db50c63a429497493a295393a55f73ff9
hrihoryeu <egoragrig@gmail.com>
2021-09-14 16:38:33 +0300

    fix: fix permissions for customer

--- customer/api/v1/views.py
+++ customer/api/v1/views.py
@@ -1,20 +1,25 @@
+from .serializers import CustomerSerializer, OfferSerializer, UserSerializer
+from .permissions import IsAdminOrViewOnly
+
 from customer.models import Customer, Offer, User
-from customer.api.v1.serializers import CustomerSerializer, OfferSerializer, UserSerializer
 from core.views import AbstractViewSet
 
 from rest_framework.viewsets import ModelViewSet
 
 
 class UserViewSet(ModelViewSet):
+    permission_classes = (IsAdminOrViewOnly,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class CustomerViewSet(AbstractViewSet):
+    permission_classes = (IsAdminOrViewOnly,)
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
 
 class OfferViewSet(AbstractViewSet):
+    permission_classes = (IsAdminOrViewOnly,)
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
