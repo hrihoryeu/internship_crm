45ea10b36e10166dfedc635be429489218b7d187
hrihoryeu <egoragrig@gmail.com>
2021-09-14 16:48:28 +0300

    feat: create api dir for provider app with v1 dir inside and move all relatated-to-api files inside

--- internship_crm/urls.py
+++ internship_crm/urls.py
@@ -26,7 +26,7 @@ from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/provider/', include('provider.urls')),
+    path('api/provider/', include('provider.api.v1.urls')),
     path('api/car_showroom/', include('car_showroom.urls')),
     path('api/customer/', include('customer.api.v1.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
--- provider/api/v1/views.py
+++ provider/api/v1/views.py
@@ -1,4 +1,4 @@
-from .models import (
+from provider.models import (
     Car,
     Provider,
     ProviderCar,
