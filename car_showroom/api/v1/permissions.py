d54ea31c19fb52a9d9623483b9629d7353d3d6d0
hrihoryeu <egoragrig@gmail.com>
2021-09-17 14:54:33 +0300

    fix: simplify serializer

--- car_showroom/api/v1/permissions.py
+++ car_showroom/api/v1/permissions.py
@@ -2,18 +2,10 @@ from rest_framework import permissions
 
 
 class IsAdminOrViewOnly(permissions.BasePermission):
-    edit_methods = ("PUT", "PATCH")
-
     def has_permission(self, request, view):
-        if request.user.is_authenticated and request.user.is_superuser:
-            return True
-
-        if request.user.is_authenticated and request.method not in ['POST', ]:
+        if request.user.is_authenticated and request.user.is_superuser and request.method != 'POST':
             return True
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and request.user.is_superuser:
-            return True
-
-        if request.user.is_authenticated and request.method == 'GET':
+        if request.user.is_authenticated and request.user.is_superuser and request.method == 'GET':
             return True
